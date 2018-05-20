19f068e6283153e30923acbc7b6155e1718d7917
Alex McKinney <mensch@uber.com>
2018-05-19 19:05:59 -0700

    Start running the gRPC server

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
 
-func main() { fmt.Println("LSM-Go") }
+	"github.com/amckinney/lsm-go/gen/idl/lsm-go"
+	"github.com/amckinney/lsm-go/handler/lsm"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	server := grpc.NewServer()
+	handler, err := lsm.New()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	lsmgopb.RegisterLSMGoServer(server, handler)
+	server.Serve(listener)
+}
